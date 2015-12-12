0c290126da45a6b832631537355881f6da6bb09e
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-12-12 18:56:01 +0100

    Fixed issue

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ console.log(process.env.smoqToken);
 var express = require('express');
 var app = express(),
     bodyParser = require('body-parser');
+var request = require('request');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -21,12 +22,21 @@ app.get('/', function (req, res) {
     res.send('Smoq Bot!');
 });
 
-app.post('/webhook/:token', function (req, res)
-{
+app.post('/webhook/:token', function (req, res) {
     if (process.env.smoqToken == req.params.token) {
         console.log('Yup');
         console.log(req.params);
         console.log(req.body);
+
+        request.post('https://api.telegram.org/bot' + process.env.smoqToken +
+            '/sendMessage', {
+            chat_id: req.body.message.chat.id,
+            text: req.body.message.text
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body) // Show the HTML for the Google homepage.
+            }
+            );
     } else {
         console.log('Nope');
     }
