5d703cec1bd3cd637ba44d4f6860c26bd0bc211c
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-12-12 16:50:26 +0100

    Created simple server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log(process.env.smoqToken);
+
+
+var express = require('express');
+var app = express();
+
+
+app.get('/', function (req, res) {
+    res.send('Smoq Bot!');
+});
+
+var server = app.listen(process.env.smoqPort, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Smoq Bot listening at http://%s:%s', host, port);
+});
