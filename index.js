35b6cc3720502b82b1b1d29ca5998931f8607490
Przemysław Płyś <przemyslaw.plys@gmail.com>
2015-12-12 17:03:41 +0100

    Implemented json body parser

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ console.log(process.env.smoqToken);
 
 
 var express = require('express');
-var app = express();
+var app = express(),
+    bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 
 app.get('/', function (req, res) {
     res.send('Smoq Bot!');
 });
+
+app.post('/:token', function (req, res))
+{
+
+    if (proces.env.smoqToken.equals(req.params.token)) {
+        console.log('Yup');
+        console.log(req.params);
+        console.log(req.body);
+    } else {
+        console.log('Nope');
+    }
+    res.send();
+}
 
 var server = app.listen(process.env.smoqPort, function () {
     var host = server.address().address;
